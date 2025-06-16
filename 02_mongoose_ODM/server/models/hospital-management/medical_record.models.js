8bb253a809528709eb2a18040361ffdc2a5255ad
milind55555 <hanchatemilind291@gmail.com>
2025-06-16 16:13:58 +0530

    Hospital-management System model and schema added

---
+++ 02_mongoose_ODM/server/models/hospital-management/medical_record.models.js
@@ -0,0 +1,7 @@
+import mongoose from "mongoose"
+
+const medicalrecordSchema=new mongoose.Schema(
+    {}
+    ,{timestamps:true})
+
+export const MedicalRecord=mongoose.model("MedicalRecord",medicalrecordSchema)    
